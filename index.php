3fc5994d90cc74da4b91d59d3887995c11e71b33
Pon Pon <aieponsike1045@gmail.com>
2025-07-04 10:12:16 +0630

    This is error sytax

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+    <h1>Hello my name is pon pon</h1>
+    <h3>This is error sytax</h3>
+</body>
+</html>
